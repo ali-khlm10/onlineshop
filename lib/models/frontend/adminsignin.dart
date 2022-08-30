e531851aca74471a643cee2549c088c6f53c0c2e
Aliraza Khodaei <khodaei.alireza76@gmail.com>
2022-08-31 01:29:22 +0430

    modified this file

--- lib/models/frontend/adminsignin.dart
+++ lib/models/frontend/adminsignin.dart
@@ -13,85 +13,92 @@ class _adminSigninState extends State<adminSignin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          margin: EdgeInsets.symmetric(
-            horizontal: widget.size.width * .2,
-            vertical: widget.size.height * .18,
-          ),
-          width: 350,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            color: Colors.teal,
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            // crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: widget.size.height * 0.05,
-              ),
-              const Text('پنل مدیریت سایت'),
-              SizedBox(
-                height: widget.size.height * 0.08,
-              ),
-              Container(
-                alignment: Alignment.centerRight,
-                child: const Text(
-                  'نام کاربری',
-                  textAlign: TextAlign.end,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            margin: EdgeInsets.symmetric(
+              horizontal: widget.size.width * .2,
+              vertical: widget.size.height * .18,
+            ),
+            width: 350,
+            height: 380,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.teal,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: widget.size.height * 0.05,
                 ),
-              ),
-              SizedBox(
-                height: widget.size.height * 0.01,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(horizontal: 20),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
+                const Text('پنل مدیریت سایت'),
+                SizedBox(
+                  height: widget.size.height * 0.08,
+                ),
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: const Text(
+                    'نام کاربری',
+                    textAlign: TextAlign.end,
                   ),
                 ),
-              ),
-              SizedBox(
-                height: widget.size.height * 0.01,
-              ),
-              Container(
-                alignment: Alignment.centerRight,
-                child: const Text(
-                  'رمز عبور',
-                  textAlign: TextAlign.end,
+                SizedBox(
+                  height: widget.size.height * 0.01,
                 ),
-              ),
-              SizedBox(
-                height: widget.size.height * 0.01,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 20),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: widget.size.height * 0.04,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => panelmanagement(size: widget.size),
+                TextField(
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
                     ),
-                  );
-                },
-                child: const Center(
-                  child: Text('ورود'),
+                  ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: widget.size.height * 0.01,
+                ),
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: const Text(
+                    'رمز عبور',
+                    textAlign: TextAlign.end,
+                  ),
+                ),
+                SizedBox(
+                  height: widget.size.height * 0.01,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: widget.size.height * 0.04,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            panelmanagement(size: widget.size),
+                      ),
+                    );
+                  },
+                  child: const Center(
+                    child: Text('ورود'),
+                  ),
+                ),
+                SizedBox(
+                  height: 60,
+                )
+              ],
+            ),
           ),
         ),
       ),
