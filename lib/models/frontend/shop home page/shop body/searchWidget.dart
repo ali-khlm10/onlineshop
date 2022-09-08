28511ce051ab15632274ff0ae4d1e1baabbd4ee1
Aliraza Khodaei <khodaei.alireza76@gmail.com>
2022-09-08 18:17:59 +0430

    connected part of search to database

--- lib/models/frontend/shop home page/shop body/searchWidget.dart
+++ lib/models/frontend/shop home page/shop body/searchWidget.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:onlineshop/models/backend/Sqlite%20Functions/mobile_func.dart';
+import 'package:onlineshop/models/frontend/constants.dart';
+import 'package:onlineshop/models/frontend/provider.dart';
+import 'package:provider/provider.dart';
 
 class search extends StatefulWidget {
   const search({Key? key, required this.size}) : super(key: key);
@@ -8,86 +12,73 @@ class search extends StatefulWidget {
 }
 
 class _searchState extends State<search> {
+  TextEditingController textcontroller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: widget.size.height * .15,
-      margin: const EdgeInsets.all(20),
-      // decoration: BoxDecoration(
-      //   borderRadius: BorderRadius.circular(10),
-      //   border: Border.all(
-      //     color: Colors.grey,
-      //     width: 1,
-      //   ),
-      //   // color: Colors.teal[100],
-      // ),
-      child: Column(
-        children: [
-          Expanded(
-            flex: 1,
-            child: Container(
-              margin: const EdgeInsets.only(right: 10),
-              alignment: Alignment.centerRight,
-              child: const Text(
-                ': جستجوی کالای مورد نظر',
-                style: TextStyle(
-                  fontSize: 16,
+    return Consumer<listofcategorysubjectProvider>(
+      builder: (context, value, child) => Container(
+        height: widget.size.height * .15,
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                margin: const EdgeInsets.only(right: 10),
+                alignment: Alignment.centerRight,
+                child: const Text(
+                  ': جستجوی کالای مورد نظر',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.search,
-                      size: 30,
-                    ),
-                  ),
-                ),
-                // const SizedBox(
-                //   width: 5,
-                // ),
-                Expanded(
-                  flex: 5,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.symmetric(horizontal: 15),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
+            Expanded(
+              flex: 2,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: IconButton(
+                      onPressed: () async {
+                        showProductsforMobile =
+                            await getBrandMobile(textcontroller.text);
+                        value.updateshowListformobile();
+                        textcontroller.clear();
+                      },
+                      icon: const Icon(
+                        Icons.search,
+                        size: 30,
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-              ],
+                  // const SizedBox(
+                  //   width: 5,
+                  // ),
+                  Expanded(
+                    flex: 5,
+                    child: TextField(
+                      controller: textcontroller,
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(horizontal: 15),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
-    // SizedBox(
-    //   width: 150,
-    //   child: Center(
-    //     child: TextField(
-    //       decoration: InputDecoration(
-    //         contentPadding: const EdgeInsets.symmetric(horizontal: 20),
-    //         border: OutlineInputBorder(
-    //           borderRadius: BorderRadius.circular(15),
-    //         ),
-    //         isDense: true,
-    //       ),
-    //       maxLines: 2,
-    //     ),
-    //   ),
-    // );
   }
 }
